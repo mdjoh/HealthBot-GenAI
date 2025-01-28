5d3de26d080f91a31ea6bde7b8ff0e5573915791
mdjoh <marchiano.oh@concordia.ca>
2025-01-27 23:27:53 -0500

    updated preprocessing script & added full JSONL training dataset

--- fine_tuning_data/fine_tuning_data_preprocessing.py
+++ fine_tuning_data/fine_tuning_data_preprocessing.py
@@ -2,49 +2,57 @@
 # Convert csv dataset into JSONL format containing question-answer pairs for
 # assistant (disease) and user (symptoms) chat
 
-# ======================= DATA SETUP ================================
+# =============================== DATA SETUP ===================================
 import pandas as pd
 import json
-import random
 
 # read in disease & symptom dataset
-data_df = pd.read_csv('./fine_tuning_data/DiseaseAndSymptoms.csv', header=0)
+data_df = pd.read_csv('./DiseaseAndSymptoms.csv', header=0)
 
 # convert disease & symptom data into string format
 data_df = data_df.astype(str)
 
-# put column names of disease & symptom dataset in a list
-symptom_col_names = list(data_df.columns)
-symptom_col_names.remove('Disease') # remove 'Disease' element from column name list to only have symptom column names remaining
+# parse data_df into disease and symptom dataframe
+disease_df = data_df['Disease']
+symptom_df = data_df.drop(columns=['Disease'])
 
 # ======================= DATASET PREPROCESSING ================================
 # make text preprocessing functions
-# function to remove space character from symptom
-def removeSpace(symptom):
-    return symptom.replace(' ', '')
 
-# function to remove space character from symptom
-def replaceUnderscore(symptom):
-    return symptom.replace('_', ' ')
+# DISEASE:
+# function to make disease names lowercase except for hepatitis type and GERD
+def processDiseaseData(disease):
 
-# apply text preprocessing functions to disease & symptom dataframe
+    # remove leading and training spaces in disease name
+    disease = disease.strip()
 
-# make all data lowercase
-data_df = data_df.apply(lambda x: x.str.lower())
+    if disease.lower().startswith("hepatitis"):
+        disease = "hepatitis " + disease[-1].upper() # last character in a disease string that starts with 'hepatitis' is the type; ensure type is uppercase
+    elif disease != "GERD" and disease != "AIDS": # don't make 'GERD' and 'AIDS' lowercase
+        disease = disease.lower()
 
-# remove space and replace underscore character with a space character for symptom features
+    return disease
+
+# apply disease data preprocessing function
+disease_df = disease_df.apply(processDiseaseData)
+
+# SYMPTOM:
+# function to make symptom data lowercase and remove leading and trailing spaces and underscore character from a symptom string in symptom dataframe
+def processSymptomData(symptom):
+    symptom = symptom.strip()
+    symptom = symptom.lower()
+    symptom = symptom.replace('_', ' ')
+
+    return symptom
+
+# put symptom column names in a list
+symptom_col_names = list(symptom_df.columns)
+
+# apply symptom data preprocessing function to all symptom columns
 for col_name in symptom_col_names:
-    data_df[col_name] = data_df[col_name].apply(removeSpace)
-    data_df[col_name] = data_df[col_name].apply(replaceUnderscore)
+    symptom_df[col_name] = symptom_df[col_name].apply(processSymptomData)
 
-# ======================= PARSE DATASET ================================
-# make disease into its own list for assistant element of conversation
-disease_list = data_df['Disease'].values.tolist()
-
-# drop disease column from data_df to leave with only symptom features
-symptom_df = data_df.drop(columns=['Disease'])
-
-# ==================== AGGREGATE USER CHAT MESSAGES ==========================
+# ========================== AGGREGATE USER CHAT MESSAGES ================================
 # function to concatenate all symptom strings separated by ', '
 # take in a dataframe row series of symptoms which are each a string
 # take the row series values and make a list of symptoms without nan
@@ -59,15 +67,18 @@ def concatenateSymptoms(row_series):
 
     return concatenated_symptoms
 
+# get the number of rows in dataset which will be the number of chat entries in training data
+num_rows = len(symptom_df)
+
+# make disease into its own list for assistant element of conversation
+disease_list = disease_df.values.tolist()
+
 # set up empty list to append symptom strings to
 training_data_symptoms = []
 
-# get the number of rows in disease & symptom dataset
-num_rows = len(disease_list)
-
 # iterate over data_df rows and call concatenate_symptoms function in each iteration
 for row in range(num_rows):
-    symptom_string = concatenateSymptoms(data_df.iloc[row])
+    symptom_string = concatenateSymptoms(symptom_df.iloc[row])
 
     # append return string to the user symptom list
     training_data_symptoms.append(symptom_string)
@@ -75,12 +86,7 @@ for row in range(num_rows):
 # concatenate list of diseases and training data symptoms to generate a symptoms-disease tuple list
 training_data = list(zip(training_data_symptoms, disease_list))
 
-# ============================= WRITE CHAT TO A JSONL FILE =================================
-# randomly select 20 samples from symptoms-disease tuple list for use in LLM fine-tuning
-# only using 20 samples for fine-tuning to limit cost; if desired, can use more training data samples
-number_of_samples = 20
-data_to_write = random.sample(training_data, number_of_samples)
-
+# ================================== WRITE CHAT TO A JSONL FILE ======================================
 # write the symptoms-disease pairs to the JSONL output file in write mode
 with open('./fine_tuning_data/chat_data_for_fine_tuning.jsonl', 'w') as file:
 
@@ -89,15 +95,15 @@ with open('./fine_tuning_data/chat_data_for_fine_tuning.jsonl', 'w') as file:
 
     # iterate over each row of disease & symptom dataset and write each question-answer pair of a
     # user-assistant chat to the output JSONL file
-    for i in range(number_of_samples):
+    for i in range(num_rows):
         chat_format = {"messages": [
                     {"role": "system", "content": "You are a medical professional. Try to give the most likely diagnosis based on the symptoms entered by the user."},
-                    {"role": "user", "content": "What could I have if I am experiencing " + data_to_write[i][symptoms_col_num] + "?"},
-                    {"role": "assistant", "content": data_to_write[i][disease_col_num]}
+                    {"role": "user", "content": "What could I have if I am experiencing " + training_data[i][symptoms_col_num] + "?"},
+                    {"role": "assistant", "content": training_data[i][disease_col_num]}
                 ]}
 
         # write formatted chat to JSONL file
-        if i == number_of_samples - 1:
+        if i == num_rows - 1:
             file.write(json.dumps(chat_format)) # can't have a new line at the end in output file for fine-tuning
         else:
             file.write(json.dumps(chat_format) + '\n')
