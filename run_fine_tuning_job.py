004d69f1661e4884118320bea817da69abfe6500
mdjoh <marchiano.oh@concordia.ca>
2025-01-27 23:42:04 -0500

    updated training data JSONL file name to upload

--- run_fine_tuning_job.py
+++ run_fine_tuning_job.py
@@ -6,7 +6,7 @@ from fine_tuning_functions import createOpenAIClient, uploadFineTuningData, crea
 client = createOpenAIClient()
 
 # upload supplementary dataset for model fine-tuning to OpenAI platform
-training_file_id = uploadFineTuningData('./fine_tuning_data/df_chat_data_for_fine_tuning.jsonl', client)
+training_file_id = uploadFineTuningData('./fine_tuning_data/chat_data_for_fine_tuning.jsonl', client)
 
 # create and launch fine-tuning job
 fine_tuning_job_id = createFineTuneJob(client, training_file_id, model="gpt-4o-mini-2024-07-18")
